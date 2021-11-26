0f5132788c4b2d7474884f4947b5a1a06728a97c
Truong Son Mai <sonmt.work00@gmail.com>
2021-11-26 11:01:53 +0700

    init project

--- lib/constants.dart
+++ lib/constants.dart
@@ -13,3 +13,16 @@ const postContentFontSize = 16.0;
 const lightFontWeight = FontWeight.w200;
 const normalFontWeight = FontWeight.normal;
 const boldFontWeight = FontWeight.bold;
+
+
+const hostname = "http://localhost:8000";
+//API For Users
+const userSignUpEndpoint = "/api/v1/users/register/";
+const userLogInEndpoint = "/api/v1/users/login/";
+const userGetInforEndpoint = "/api/v1/users/show";
+
+//API For Friend
+const friendGetListEndpoint = "/api/v1/friends/list";
+
+//API For Post
+const postGetListEndpoint = "/api/v1/posts/list";
