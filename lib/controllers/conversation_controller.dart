69523b15797c33ade7d25a02ba8375e223a322d0
huydang2k <huydang2k@gmail.com>
2021-12-05 01:06:53 +0700

    chat

--- lib/controllers/conversation_controller.dart
+++ lib/controllers/conversation_controller.dart
@@ -1,23 +1,38 @@
 import '../models/conversation_model.dart';
+import '../models/message_model.dart';
 import 'package:http/http.dart' as http;
 import '../models/user_model.dart';
 import '../util/util.dart';
 import 'package:fluttergram/constants.dart';
 import 'dart:convert';
 final String getChatsURL = hostname + '/api/v1/chats/getChats';
+final String getMessagesURL = hostname + '/api/v1/chats/getMessages';
 // final String searchPostsURL = hostname + '/api/v1/posts/search';
-Conversation conversationFromRespJson(json){
-  final String name, messagePreview, avatar, lastMessageTime;
+Conversation conversationFromRespJson(json,messages){
+  final String id,name, messagePreview, avatar, lastMessageTime;
   final bool isActive, isSeen;
-  final List<String> messages;
-  name = json['partnerName'];
-  avatar = json['partnerAvatar'];
-  messagePreview = json['latestMessage'];
-  lastMessageTime = timeAgo(json['updatedAt']);
+  id = json["_id"] ?? '';
+  name = json['partnerName'] ?? '';
+  avatar = json['partnerAvatar'] ?? '';
+  messagePreview = json['latestMessage'] ?? '';
+  lastMessageTime = timeAgo(json['updatedAt']) ?? '';
   isActive = true;
   isSeen = false;
-  messages = [];
-  return new Conversation(name: name,messagePreview: messagePreview,avatar: avatar,lastMessageTime: lastMessageTime,messages: messages,isActive: isActive,isSeen: isSeen);
+
+  return new Conversation(id: id,name: name,messagePreview: messagePreview,avatar: avatar,lastMessageTime: lastMessageTime,messages: messages,isActive: isActive,isSeen: isSeen);
+}
+
+Message messageFromRespJson(json){
+  final String chatId,id;
+  final String? content,createdAt,updatedAt;
+  final User? user;
+  id = json["_id"] ?? '';
+  chatId = json['chat'] ?? '';
+  content = json['content'] ?? '';
+  createdAt = json['createdAt'] ?? '';
+  updatedAt = json['updatedAt'] ?? '';
+  user= User.fromJson(jsonConvert(json['user']));
+  return new Message(id: id, chatId: chatId,content: content,createdAt: createdAt,updatedAt: updatedAt,user: user);
 }
 
 Future<List<Conversation>> getConversations() async {
@@ -39,7 +54,8 @@ Future<List<Conversation>> getConversations() async {
     for (var element in resp['data'])
       // resp['data'].foreach((element)
         {
-      ls.add(conversationFromRespJson(element));
+          var messages = await getMessages(element['_id'],0);
+      ls.add(conversationFromRespJson(element,messages));
     };
     // );
 
@@ -47,75 +63,34 @@ Future<List<Conversation>> getConversations() async {
   }
 
   else return [];
-  return [
-    Conversation(
-      name: "Jenny Wilson",
-      messagePreview: "Hope you are doing well after that ",
-      avatar: "assets/images/user.png",
-      lastMessageTime: "3m",
-      isActive: false,
-      isSeen: false,
-    ),
-    Conversation(
-      name: "Esther Howard",
-      messagePreview: "Hello Abdullah! I'm from CS50 class",
-      avatar: "assets/images/user_2.png",
-      lastMessageTime: "8m",
-      isActive: true,
-      isSeen: true,
-    ),
-    Conversation(
-      name: "Ralph Edwards",
-      messagePreview: "Do you have update about the final project?",
-      avatar: "assets/images/user_3.png",
-      lastMessageTime: "5d",
-      isActive: false,
-      isSeen: true,
-    ),
-    Conversation(
-      name: "Jacob Jones",
-      messagePreview: "You’re welcome :)",
-      avatar: "assets/images/user_4.png",
-      lastMessageTime: "5d",
-      isActive: true,
-      isSeen: true,
-    ),
-    Conversation(
-      name: "Albert Flores",
-      messagePreview: "Thanks",
-      avatar: "assets/images/user_5.png",
-      lastMessageTime: "6d",
-      isActive: false,
-      isSeen: false,
-    ),
-    Conversation(
-      name: "Jenny Wilson",
-      messagePreview: "Hope you are doing well after that ",
-      avatar: "assets/images/user.png",
-      lastMessageTime: "3m",
-      isActive: false,
-      isSeen: false,
-    ),
-    Conversation(
-      name: "Esther Howard",
-      messagePreview: "Hello Abdullah! I'm from CS50 class",
-      avatar: "assets/images/user_2.png",
-      lastMessageTime: "8m",
-      isActive: true,
-      isSeen: true,
-    ),
-    Conversation(
-      name: "Ralph Edwards",
-      messagePreview: "Do you have update about the final project?",
-      avatar: "assets/images/user_3.png",
-      lastMessageTime: "5d",
-      isActive: false,
-      isSeen: true,
-    ),
-  ];
-}
 
+}
+Future<List<Message>> getMessages(chatId,page) async {
+  String token = await getToken();
+  final response = await http
+      .get(Uri.parse(getMessagesURL + '/${chatId}?page=${page}'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+      'authorization': 'bearer ' + token,
+    },
+  );
+  var resp = jsonDecode(response.body);
+  print(resp);
+
+  if (checkMessageResponse(resp['message'])) {
+    var ls = <Message>[];
+    // for element
+    for (var element in resp['data'])
+      // resp['data'].foreach((element)
+        {
+      ls.add(messageFromRespJson(element));
+    };
+    // );
+    return ls;
+  }
+  else return [];
+}
 Future<Conversation> getConversation({required User user}) async {
   return Conversation(
-      name: user.username, messages: ["Hi there", "Hi", "How are you doing?"]);
+      id: '1',name: user.username, messages: []);
 }
