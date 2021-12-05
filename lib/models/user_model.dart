cd1aaddd8987542c6b34da4c1abfdda551f2f4b1
huydang2k <huydang2k@gmail.com>
2021-12-05 13:19:56 +0700

    fix login and user model

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -6,7 +6,6 @@ class User {
   String username;
   Image? avatar;
   Image? cover_image;
-  String? token;
   String password;
   String? birthday;
   String? description;
@@ -23,7 +22,6 @@ class User {
     required this.password,
     this.avatar,
     this.cover_image,
-    this.token,
     this.birthday,
     this.address,
     this.description,
@@ -44,7 +42,6 @@ class User {
       password: '',
       avatar: Image.fromJson(jsonConvert(json['avatar'])),
       cover_image: Image.fromJson(jsonConvert(json['cover_image'])) ,
-      token: json['token'] ?? '',
       birthday: json['birthday'] ?? 'Unknown',
       description: json['description'] ?? '',
       address: json['address'] ?? '',
@@ -61,7 +58,7 @@ class User {
 
   @override
   String toString() {
-    return 'User{id: $id, phone: $phone, username: $username, avatar: $avatar, cover_image: $cover_image, token: $token, password: $password, birthday: $birthday, description: $description, address: $address, city: $city, country: $country, link: $link, gender: $gender, blocked_inbox: $blocked_inbox}';
+    return 'User{id: $id, phone: $phone, username: $username, avatar: $avatar, cover_image: $cover_image,  password: $password, birthday: $birthday, description: $description, address: $address, city: $city, country: $country, link: $link, gender: $gender, blocked_inbox: $blocked_inbox}';
   }
 
   factory User.getFriend(Map<String, dynamic> json){
@@ -84,7 +81,7 @@ class User {
       password: '',
       avatar: Image.fromJson(jsonConvert(json['avatar'])),
       cover_image: Image.fromJson(jsonConvert(json['cover_image'])) ,
-      token: json['token'] ?? '',
+
       birthday: json['birthday'] ?? 'Unknown',
       description: json['description'] ?? '',
       address: json['address'] ?? '',
